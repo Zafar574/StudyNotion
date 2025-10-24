662836972021ece2fffd5fc203b1f17bdb07d5ba
Zafar574 <zk733449@gmail.com>
2025-10-24 23:48:14 +0530

    Catalog Page created

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -22,6 +22,7 @@ import { ACCOUNT_TYPE } from "./utils/constants";
 import AddCourse from "./components/core/Dashboard/AddCourses";
 import MyCourses from "./components/core/Dashboard/MyCourses";
 import EditCourse from './components/core/Dashboard/EditCourse';
+import Catalog from './pages/Catalog';
 function App() {
     const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -32,6 +33,7 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="catalog/:catalogName" element={<Catalog />} />
         <Route
         path="signup"
         element={
